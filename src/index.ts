34ddf74b4dab823f9888ae12b683c3efb5d5e27f
Taras <sty.brest@gmail.com>
2023-02-23 16:57:51 +0100

    HTTPS temporarly disabled

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,12 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
-const securePort = process.env.SECURE_PORT || 3003;
+//const securePort = process.env.SECURE_PORT || 3003;
 
-const secureServerOptions = {
+/*const secureServerOptions = {
   key: fs.readFileSync(process.env.KEY || './keys/local.key', 'utf8'),
   cert: fs.readFileSync(process.env.CERT || './keys/local.crt', 'utf8'),
-};
+};*/
 
 const whitelist = ['http://semenyuk.eu', 'https://semenyuk.eu', 'http://localhost'];
 const corsOptions: CorsOptions = {
@@ -40,6 +40,6 @@ app.use(cors(corsOptions));
 app.use('/', router);
 
 http.createServer(app).listen(port, () => console.log(`server runs on port ${port}`));
-https
+/*https
   .createServer(secureServerOptions, app)
-  .listen(securePort, () => console.log(`secure server runs on port ${securePort}`));
+  .listen(securePort, () => console.log(`secure server runs on port ${securePort}`));*/
